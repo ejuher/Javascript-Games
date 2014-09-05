b0fb798e9aebdef1f5b2f5de416a6dc19611cafe
Erich Uher <ejuher@gmail.com>
2014-09-05 11:29:42 -0700

    initial commit

---
+++ clock.js
@@ -0,0 +1,42 @@
+function Clock () {
+}
+
+Clock.TICK = 5000;
+
+Clock.prototype.printTime = function () {
+  // Format the time in HH:MM:SS
+  var date = new Date(this.currentTime);
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var timeString = hours + ":" + minutes + ":" + seconds;
+  console.log(timeString);
+};
+
+Clock.prototype.run = function () {
+  // 1. Set the currentTime.
+  // 2. Call printTime.
+  // 3. Schedule the tick interval.
+  this.currentTime = Date.now();
+  this.printTime();
+  
+  var that = this;
+  setTimeout( function () {
+    that._tick();
+  }, Clock.TICK);
+};
+
+Clock.prototype._tick = function () {
+  // 1. Increment the currentTime.
+  // 2. Call printTime.
+  this.currentTime += Clock.TICK;
+  this.printTime();
+  
+  var that = this;
+  setTimeout( function () {
+    that._tick();
+  }, Clock.TICK);
+};
+
+clock = new Clock();
+clock.run();
